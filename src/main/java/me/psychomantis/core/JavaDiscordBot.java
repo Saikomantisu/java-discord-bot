67aaea7b5bc06156a78ce735991a6b7e0386d3dd
Ravinath Akalanka <rajaravinath0@gmail.com>
2024-12-09 01:41:10 +0000

    info: changing the main class name

--- src/main/java/me/psychomantis/core/JavaDiscordBot.java
+++ src/main/java/me/psychomantis/core/JavaDiscordBot.java
@@ -1,6 +1,5 @@
 package me.psychomantis.core;
 
-import javax.security.auth.login.LoginException;
 import me.psychomantis.commands.*;
 import me.psychomantis.listeners.BotListener;
 import me.psychomantis.managers.CommandManager;
@@ -10,7 +9,7 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
-public class NoizeBot {
+public class JavaDiscordBot {
 
     static JDA builder;
     static CommandManager manager;
